ee65d675ea238d48510ed34d5ebdeaf0a6a3db75
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-05-09 12:41:16 +0530

    Update rsa_encryption.rs

---
+++ aes-rsa-rust/src/rsa_encryption.rs
@@ -0,0 +1,22 @@
+use rsa::{RsaPrivateKey, RsaPublicKey, PaddingScheme};
+use rand::rngs::OsRng;
+use base64::{encode, decode};
+
+pub fn rsa_demo() {
+    let mut rng = OsRng;
+    let bits = 2048;
+
+    let private_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate key");
+    let public_key = RsaPublicKey::from(&private_key);
+
+    let message = b"Top secret!";
+    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+
+    // Encrypt
+    let enc_data = public_key.encrypt(&mut rng, padding, &message[..]).expect("encryption failed");
+    println!("[RSA] Encrypted: {}", encode(&enc_data));
+
+    // Decrypt
+    let dec_data = private_key.decrypt(padding, &enc_data).expect("decryption failed");
+    println!("[RSA] Decrypted: {}", String::from_utf8(dec_data).unwrap());
+}
